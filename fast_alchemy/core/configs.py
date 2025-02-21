2e7dd348d241adf91d605eb0382deaad7c62fe32
joão pedro <jpmmoliveira@gmail.com>
2025-02-21 10:37:11 -0400

    tentando conectsar essa bomba

--- fast_alchemy/core/configs.py
+++ fast_alchemy/core/configs.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
 # configurações  gerais usadas na aplicação
 
  API_V1_STR: str = '/api/v1'
- DB_URL: str = "postgresql+asyncpg://postgres:ssW1T68NHbWpNgLH@abundantly-luminous-frog.data-1.use1.tembo.io:5432/postgres"
+ DB_URL: str = "postgresql+asyncpg://postgres:ssW1T68NHbWpNgLH@abundantly-luminous-frog.data-1.use1.tembo.io:5432/adocaogatos"
  DBBaseModel = declarative_base()
 
 
